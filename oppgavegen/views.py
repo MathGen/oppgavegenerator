767fa72f5adc89ccfdaa89e9e32f5db29060fc2e
einar <qeinar@gmail.com>
2015-01-12 12:44:06 +0100

    testing

--- oppgavegen/views.py
+++ oppgavegen/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import Context, loader
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 # Create your views here.
+
+def index(request):
+    #template = loader.get_template('index.html')
+    context = RequestContext(request)
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render_to_response('Index', context_dict, context)
--- oppgavegenerator/urls.py
+++ oppgavegenerator/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'oppgavegenerator.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'oppgavegen.views.index', name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
