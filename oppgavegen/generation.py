425952181a47d91891a6203014143d7cf03e6cb1
einar <qeinar@gmail.com>
2015-01-13 11:21:53 +0100

    Cleaned up a bit

---
+++ oppgavegen/generation.py
@@ -0,0 +1,20 @@
+from random import randint
+
+
+def printer():
+    string = "Spaghetti"
+    return string
+
+def arithmetics():
+    number1 = randint(0,10)
+    number2 = randint(0,10)
+    operators = ["+", "-", "*"]
+    opNumber = randint(0,2)
+    if opNumber == 0:
+        answer = number1 + number2
+    elif opNumber == 1:
+        answer = number1 - number2
+    else:
+        answer = number1 * number2
+    arr = [number1,number2,answer, operators[opNumber]]
+    return arr
--- oppgavegen/views.py
+++ oppgavegen/views.py
@@ -7,29 +7,18 @@ from random import randint
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django import forms
-
+from oppgavegen import generation
 
 # Create your views here.
 
 def index(request):
     #template = loader.get_template('index.html')
     context = RequestContext(request)
-    number1 = randint(0,10)
-    number2 = randint(0,10)
-    operators = ["+", "-", "*"]
-    opNumber = randint(0,2)
-    if opNumber == 0:
-        answer = number1 + number2
-    elif opNumber == 1:
-        answer = number1 - number2
-    else:
-        answer = number1 * number2
-
-    question = "hva er " + str(number1) + " " + operators[opNumber] + " " + str(number2)
+    arr = generation.arithmetics()
+    answer = str(arr[2])
+    question = "hva er " + str(arr[0]) + " " + arr[3]+ " " + str(arr[1])
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
         form = QuestionForm(request.POST)
-        # check whether it's valid:
         if form.is_valid():
             form_values = form.process()
             user_answer = form_values[0]
@@ -37,15 +26,15 @@ def index(request):
             if user_answer == answer:
                 answer_text = "Du har svart riktig!"
             else:
-                answer_text = "Du har svart feil. Riktig svar er: " + str(answer)
+                answer_text = "Du har svart feil. Riktig svar er: " + answer
             # redirect to a new URL:
             context_dict = {'title': "spaghetti", 'question' : question, 'answer' : answer_text, 'user_answer' : user_answer}
             return render_to_response('answers', context_dict, context)
     else:
        form = QuestionForm()
-       form.fields["answer"].initial = str(answer)
+       form.fields["answer"].initial = answer #Setter initsiell verdi til skjult felt i form.
 
-    context_dict = {'title': "spaghetti", 'question' : question, 'answer' : str(answer), 'form' : form}
+    context_dict = {'title': generation.printer(), 'question' : question, 'answer' : str(answer), 'form' : form}
     return render_to_response('index', context_dict, context)
 
 
