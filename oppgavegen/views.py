44d36b16bb7ea18b11b57587db4ebbbb41f5f8b4
einar <qeinar@gmail.com>
2015-01-15 15:20:19 +0100

    Added navigation for different question types

--- oppgavegen/views.py
+++ oppgavegen/views.py
@@ -14,7 +14,11 @@ from oppgavegen import generation
 def index(request):
     #template = loader.get_template('index.html')
     context = RequestContext(request)
-    arr = generation.altArithmetics()
+    question_type = request.GET.get('q', '')
+    if question_type == "algebra":
+        arr = generation.algebra()
+    else:
+        arr = generation.altArithmetics()
     answer = str(arr[1])
     question = arr[0]
     if request.method == 'POST':
