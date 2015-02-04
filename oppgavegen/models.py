b0578d725103f8c965b7f3a715a7072dc791c8a8
zelnoth <qeinar@gmail.com>
2015-02-04 12:46:47 +0100

    Added unicode returns in models.py for both Topic and Template

--- oppgavegen/models.py
+++ oppgavegen/models.py
@@ -4,21 +4,27 @@ from django.contrib import admin
 
 # Create your models here.
 class Topic(models.Model):
-    topic = models.CharField(max_length=200)                # Name of the topic.
-    def clean(self):
+    topic = models.CharField(max_length=200)                  # Name of the topic.
+    def __unicode__(self):                                    #Makes it so that self.topic shows up instead of topic(object)
+        return self.topic
+
+    def clean(self): #Removes trailing whitespace from topic
         if self.topic:
             self.topic = self.topic.strip()
 
 class Template(models.Model):
     question_text = models.CharField(max_length=200)          # The template. Math expression or text question ex. "Solve: ax = b + cx"
-    solution =  models.CharField(max_length=10000)               # Step by step solution to the answer
-    answer = models.CharField(max_length=200)                     # A simple math operation to calculate the problem template. Should be the last step in the solution.
-    creator = models.ForeignKey(User)                                  # User ID of creator of template
-    creation_date = models.DateTimeField('date created')       # Date and time of creation
-    variables = models.PositiveSmallIntegerField()                  # A number of how many variables are in the problem template.
-    rating = models.PositiveSmallIntegerField()                      # Difficulty rating. Defaults to 1200.
-    times_solved = models.PositiveIntegerField()                    # Amount of times the problem has been solved.
-    times_failed = models.PositiveIntegerField()                    # Amount of times the problem has not been solved (wrong answer.)
-    topic = models.ForeignKey(Topic)                                    # ID of the topic this problem belongs to ex. 2 (where 2 means algebra.)
+    solution =  models.CharField(max_length=10000)            # Step by step solution to the answer
+    answer = models.CharField(max_length=200)                 # A simple math operation to calculate the problem template. Should be the last step in the solution.
+    creator = models.ForeignKey(User)                         # User ID of creator of template
+    creation_date = models.DateTimeField('date created')      # Date and time of creation
+    variables = models.PositiveSmallIntegerField()            # A number of how many variables are in the problem template.
+    rating = models.PositiveSmallIntegerField()               # Difficulty rating. Defaults to 1200.
+    times_solved = models.PositiveIntegerField()              # Amount of times the problem has been solved.
+    times_failed = models.PositiveIntegerField()              # Amount of times the problem has not been solved (wrong answer.)
+    topic = models.ForeignKey(Topic)                          # ID of the topic this problem belongs to ex. 2 (where 2 means algebra.)
     number_of_decimals = models.PositiveSmallIntegerField()
     answer_can_be_zero = models.BooleanField(default=False)
+
+    def __unicode__(self):                                    #Makes it so that self.question_text shows up instead of topic(object)
+        return self.question_text
