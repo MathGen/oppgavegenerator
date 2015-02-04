da6034580fcdc88338150a06119c2f8de1c18c6b
Eirik Korsnes <korsneseirik@gmail.com>
2015-02-04 17:06:31 +0100

    test3

--- oppgavegenerator/settings.py
+++ oppgavegenerator/settings.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
 
 
 # Quick-start development settings - unsuitable for production
