ad5f87b1f319de699eabf3adc495f6c8322bcaa6
einar <qeinar@gmail.com>
2015-02-20 13:30:31 +0100

    Deleted the fonts folder because we did not use it.

--- static/js/answer_input.js
+++ static/js/answer_input.js
@@ -1,8 +1,27 @@
 $(document).ready(function() {
-    var radioBtn = '<input type="radio" name="rbtnCount" />';
+    var radio_btn = '<input type="radio" name="rbtnCount" />';
+    var answer_box = '<input type="radio" name="answer_box" />';
+    var template_type = 0;
 
-    for (i = 0; i < 10; i++) {
-        $('#target').append(radioBtn);
+
+    if(template_type == 'multiple') {
+        var choices = 0; //get choices.split(§)
+        for (i = 0; i < choices.size(); i++) {
+            $('#target').append(radio_btn);
+        }
     }
+    else if(template_type == 'normal'){
+        var number_of_answers = 0;
+        for (i = 0; i < number_of_answers; i++) {
+
+        }
+    }
+    else if(template_type == 'insert'){
+        var number_of_answers = 0;
+        for (i = 0; i < number_of_answers; i++) {
+
+        }
+    }
+
 
 });
