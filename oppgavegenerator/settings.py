38b8c1093ad70f4716cea01c29b95e480f35e079
Eirik Korsnes <korsneseirik@gmail.com>
2015-03-04 12:05:25 +0100

    Changed back settings.py to old staticfiles_dirs

--- oppgavegenerator/settings.py
+++ oppgavegenerator/settings.py
@@ -90,8 +90,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_DIRS = (os.path.join(os.path.dirname(__file__),'static'),)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = (os.path.join(os.path.dirname(__file__),'static'),)
+# one of these staticfiles_dirs are correct.
 
 #PATH_PROJECT = os.path.realpath(os.path.dirname(__file__))
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
