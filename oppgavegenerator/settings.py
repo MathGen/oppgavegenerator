ccb1a1834fbe1f23b6e20134b5d45c9712f25053
einar <qeinar@gmail.com>
2015-03-04 18:52:41 +0100

    added a comment to setting secrets template.

--- oppgavegenerator/settings.py
+++ oppgavegenerator/settings.py
@@ -15,9 +15,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
