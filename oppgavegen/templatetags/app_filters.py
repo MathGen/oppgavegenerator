d964376e775b8af58a8ac7ae6157498ad8580b3d
einar <qeinar@gmail.com>
2015-03-04 17:41:05 +0100

    registered is_teacher as a filter.

--- oppgavegen/templatetags/app_filters.py
+++ oppgavegen/templatetags/app_filters.py
@@ -2,7 +2,7 @@ from django import template
 from django.contrib.auth.models import Group
 register = template.Library()
 
-
+@register.filter
 def is_teacher(user):
     if user.is_superuser:
         return True
