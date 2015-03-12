95a341775d58199bd95ed63e24a3c861ace3554a
Eirik Korsnes <korsneseirik@gmail.com>
2015-03-12 15:47:52 +0100

    added "view" column in templateslist-view to go directly to specific template (for testing purposes)

--- oppgavegen/tables.py
+++ oppgavegen/tables.py
@@ -5,8 +5,11 @@ import django_tables2 as tables
 from oppgavegen.models import Template
 
 class TemplateTable(tables.Table):
+    View = tables.TemplateColumn('<a href="/?q={{record.id}}">View</a>')
+
     class Meta:
         model = Template
         attrs = {"class": "paleblue"} # add class="paleblue" (table theme) to <table> tag
         # fields to include in table (displayed in this order)
         fields = ("id","question_text", "creator", "topic", "type", "rating")
+
