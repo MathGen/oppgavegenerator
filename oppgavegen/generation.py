a3fd31a888046b719631ec3553487a83821580eb
einar <qeinar@gmail.com>
2015-04-10 15:34:25 +0200

    testing a stupid fix.

--- oppgavegen/generation.py
+++ oppgavegen/generation.py
@@ -162,6 +162,7 @@ def calculate_answer(s):
     transformations = standard_transformations + (implicit_multiplication,)
     s = str(parse_expr(s, transformations=transformations))
     s = parse_expr(str(s), transformations=implicit_application)
+    transformations = ''
     s = latex(sympify(s)) #sometimes this returns the value 'zoo' | also could maybe use simplify instead of sympify
     #s = RR(s)
     #s = round(s, 3)
