9e2368a5ea2ec18ac090b124a00c31e72f73e64b
einar <qeinar@gmail.com>
2015-04-10 15:37:18 +0200

    force everything to strings. (calculate_answer)

--- oppgavegen/generation.py
+++ oppgavegen/generation.py
@@ -161,9 +161,8 @@ def calculate_answer(s):
     s = str(latex_to_sympy(s))
     transformations = standard_transformations + (implicit_multiplication,)
     s = str(parse_expr(s, transformations=transformations))
-    s = parse_expr(str(s), transformations=implicit_application)
-    transformations = ''
-    s = latex(sympify(s)) #sometimes this returns the value 'zoo' | also could maybe use simplify instead of sympify
+    s = str(parse_expr(str(s), transformations=implicit_application))
+    s = latex(sympify(str(s))) #sometimes this returns the value 'zoo' | also could maybe use simplify instead of sympify
     #s = RR(s)
     #s = round(s, 3)
     return str(s)
