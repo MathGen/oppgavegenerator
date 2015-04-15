3ce596111e941c272656967694cff326d431cb8e
einar <qeinar@gmail.com>
2015-04-15 21:26:57 +0200

    Removed a bunch of todo's that were already done.

--- oppgavegen/generation.py
+++ oppgavegen/generation.py
@@ -495,6 +495,7 @@ def test_template(template):
 
         try: #todo: there is probably a better way to do this.
             answer = parse_answer(answer, random_domain)
+            #even if tghios is unused it still checks if parsing the solution crashes.
             solution = parse_solution(solution, random_domain)
             got_trough_test = 1
         except Exception:
