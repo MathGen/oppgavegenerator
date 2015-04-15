fcf88667f770a5b5cc59fa04c0595241a3c69d9e
w0pke <kenniii90@gmail.com>
2015-04-15 16:47:27 +0200

    Fixed a bug where \degree caused an endless loop. \degree is parsed as ^\circ and was not wrapped with {} properly.

--- static/js/gen.js
+++ static/js/gen.js
@@ -1093,8 +1093,6 @@ function convert_variables(latex){
 	var la = latex;
 	la = la.replace(/\\cdots/g, '\\cdot ');
 	la = la.replace(/\\cdot/g,'\\cdot ');
-	//la = la.replace(/\\left/g,'');
-	//la = la.replace(/\\right/g,'');
 	la = la.replace(/\\&/g, '&');
 	la = la.replace(/\\ln/g, '\\ln ');
 	la = la.replace(/\\sin/g, '\\sin ');
@@ -1111,7 +1109,13 @@ function convert_variables(latex){
 	for(var j = 0; j < la.length; j++){
 		if(la[j] == '^' || la[j] == '_'){
 			if(la[j+1] != '{'){
-				la = la.substring(0, j+1) + '{' + la[j+1] + '}' + la.substring(j+2, la.length);
+				// Bug with \circ where it was not wrapped with {}.
+				if(la[j+2] == 'c' && la[j+3] == 'i' && la[j+4] == 'r' && la[j+5] == 'c'){
+					la = la.substring(0, j+1) + '{\\circ }' + la.substring(j+6, la.length);
+				}
+				else{
+					la = la.substring(0, j+1) + '{' + la[j+1] + '}' + la.substring(j+2, la.length);
+				}
 			}
 		}
 	}
