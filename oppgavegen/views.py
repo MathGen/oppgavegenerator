5feba1cf4a6ddb805675007f6e4b8b699f4b70f4
einar <qeinar@gmail.com>
2015-05-14 18:13:27 +0200

    Pepped the shit out of view_logic.

--- oppgavegen/views.py
+++ oppgavegen/views.py
@@ -1,34 +1,22 @@
 from django.contrib.auth.decorators import login_required, user_passes_test
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import Context, loader
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from random import randint
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
 from django import forms
-from oppgavegen import generation
-#sympy
-from sympy import sympify
-from sympy.printing.mathml import mathml
-from sympy import Integral, latex
 from django.forms import ModelForm
-# models and tables
-from oppgavegen.models import Template
-from oppgavegen.models import Topic
-from oppgavegen.models import User
 from oppgavegen.tables import *
 from django_tables2 import RequestConfig
 from oppgavegen.templatetags.app_filters import is_teacher
 from oppgavegen import view_logic
 from oppgavegen.view_logic import *
 
+
 def is_member(user):
     """Returns true/false depending on if the user is a member of the teacher group (or is a superuser)"""
     if user.is_superuser:
         return True
     return user.groups.filter(name='Teacher').exists()
+
 
 @login_required
 def task(request):
@@ -42,6 +30,7 @@ def task(request):
     context_dict['rating'] = view_logic.get_user_rating(request.user)
     return render_to_response('taskview.html', context_dict, context)
 
+
 @login_required
 def task_by_id_and_type(request, template_id, desired_type='normal'):
     """Returns a render of taskview with a specific math template with specified type"""
@@ -49,9 +38,10 @@ def task_by_id_and_type(request, template_id, desired_type='normal'):
     context_dict = generation.generate_task(request.user, template_id, desired_type)
     context_dict['rating'] = view_logic.get_user_rating(request.user)
     if context_dict['question'] == 'error':
-        message = {'message' : 'Denne oppgavetypen har ikke blitt laget for denne oppgaven'}
+        message = {'message': 'Denne oppgavetypen har ikke blitt laget for denne oppgaven'}
         return render_to_response('error.html', message, context)
     return render_to_response('taskview.html', context_dict, context)
+
 
 @login_required
 def task_by_id(request, template_id):
@@ -60,6 +50,7 @@ def task_by_id(request, template_id):
     context_dict = generation.generate_task(request.user, template_id)
     context_dict['rating'] = view_logic.get_user_rating(request.user)
     return render_to_response('taskview.html', context_dict, context)
+
 
 class QuestionForm(forms.Form):
     user_answer = forms.CharField(widget=forms.widgets.HiddenInput(), max_length=400)
@@ -72,11 +63,15 @@ class QuestionForm(forms.Form):
 
     def process(self):
         """Returns a cleaned dictionary of it's own values."""
-        cd = {'variable_dictionary': self.cleaned_data['variable_dictionary'],'primary_key': self.cleaned_data['primary_key'],
-              'user_answer': self.cleaned_data['user_answer'], 'template_type': self.cleaned_data['template_type'],
+        cd = {'variable_dictionary': self.cleaned_data['variable_dictionary'],
+              'primary_key': self.cleaned_data['primary_key'],
+              'user_answer': self.cleaned_data['user_answer'],
+              'template_type': self.cleaned_data['template_type'],
               'template_specific': self.cleaned_data['template_specific'],
-              'replacing_words': self.cleaned_data['replacing_words'], 'disallowed': self.cleaned_data['disallowed']}
+              'replacing_words': self.cleaned_data['replacing_words'],
+              'disallowed': self.cleaned_data['disallowed']}
         return cd
+
 
 class TemplateForm(ModelForm):
     class Meta:
@@ -85,8 +80,9 @@ class TemplateForm(ModelForm):
 
         def process(self):
             """Returns a cleaned dictionary of it's own values."""
-            cd = [self.cleaned_data['question'], self.cleaned_data['answer']]
+            cd = {self.cleaned_data['question'], self.cleaned_data['answer']}
             return cd
+
 
 @login_required
 @user_passes_test(is_teacher, '/')
@@ -94,13 +90,13 @@ def gen(request):
     """Returns a render of gen.html"""
     context = RequestContext(request)
     topics = ""
-    for e in Topic.objects.all(): #retrieves a list of topics and passes them to the view.
+    for e in Topic.objects.all():  # Retrieves a list of topics and passes them to the view.
         topics += '§' + str(e.pk) + '§'
         topics += e.topic
     topics = topics[1:]
-    context_dict = {'topics':topics}
-    context_dict['rating'] = view_logic.get_user_rating(request.user)
+    context_dict = {'topics': topics, 'rating': view_logic.get_user_rating(request.user)}
     return render_to_response('gen.html', context_dict, context)
+
 
 @login_required
 @user_passes_test(is_teacher, '/')
@@ -113,8 +109,8 @@ def submit(request):
         form = TemplateForm(request.POST)
         if form.is_valid():
             template = form.save(commit=False)
-            if request.REQUEST['pk'] != '': #can this be written as v = req != ''?
-                template.pk = request.REQUEST['pk'] #workaround, for some reason template doesn't automatically get template.pk
+            if request.REQUEST['pk'] != '':  # Can this be written as v = req != ''?
+                template.pk = request.REQUEST['pk']  # Workaround, template doesn't automatically get template.pk
                 update = True
             else:
                 update = False
@@ -125,6 +121,7 @@ def submit(request):
     context = RequestContext(request)
     return render_to_response('submit.html', {'message': message}, context)
 
+
 @login_required
 def answers(request):
     """Returns a render of answers.html"""
@@ -132,7 +129,6 @@ def answers(request):
     cheat_message = '\\text{Ulovlig tegn har blitt brukt i svar}'
     if request.method == 'POST':
         form = QuestionForm(request.POST)
-        #todo: add a check for invalid signs and return a cheating page
         if form.is_valid():
             form_values = form.process()
             template = Template.objects.get(pk=form_values['primary_key'])
@@ -144,7 +140,8 @@ def answers(request):
             return render_to_response('answers.html', context_dict, context)
         else:
             print(form.errors)
-    return  render_to_response('answers.html')
+    return render_to_response('answers.html')
+
 
 @login_required
 @user_passes_test(is_teacher, '/')
@@ -154,6 +151,7 @@ def templates(request):
     table = BootstrapTemplateTable(Template.objects.filter(valid_flag=True))
     RequestConfig(request, paginate={"per_page": 20}).configure(table)
     return render(request, "tableview.html", {"table": table, "panel_title": panel_title})
+
 
 @login_required
 @user_passes_test(is_teacher, '/')
@@ -165,6 +163,7 @@ def template_table_by_user(request):
     RequestConfig(request, paginate={"per_page": 20}).configure(table)
     return render(request, "tableview.html", {"table": table, "panel_title": panel_title})
 
+
 @login_required
 @user_passes_test(is_teacher, '/')
 def user_overview_table(request):
@@ -174,20 +173,21 @@ def user_overview_table(request):
     RequestConfig(request, paginate={"per_page": 20}).configure(table)
     return render(request, "tableview.html", {"table": table, "panel_title": panel_title})
 
+
 @login_required
 @user_passes_test(is_teacher, '/')
 def new_template(request):
     """Returns a render of newtemplate.html used for creating new templates"""
     context = RequestContext(request)
-    #retrieves a list of topics and passes them to the view.
+    # Retrieves a list of topics and passes them to the view.
     topics = ""
     for e in Topic.objects.all():
         topics += '§' + str(e.pk) + '§'
         topics += e.topic
     topics = topics[1:]
-    context_dict = {'topics':topics}
-    context_dict['rating'] = view_logic.get_user_rating(request.user)
+    context_dict = {'topics': topics, 'rating': view_logic.get_user_rating(request.user)}
     return render_to_response('newtemplate.html', context_dict, context)
+
 
 @login_required
 @user_passes_test(is_teacher, '/')
@@ -197,4 +197,3 @@ def edit_template(request, template_id):
     context_dict = view_logic.make_edit_context_dict(template_id)
     context_dict['rating'] = view_logic.get_user_rating(request.user)
     return render_to_response('edit.html', context_dict, context)
-
