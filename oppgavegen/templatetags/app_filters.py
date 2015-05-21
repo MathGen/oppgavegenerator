3fa0baa1200e5420a28c65112d595f1bfae0453a
einar <qeinar@gmail.com>
2015-05-21 16:15:05 +0200

    Added docstrings for filters.

--- oppgavegen/templatetags/app_filters.py
+++ oppgavegen/templatetags/app_filters.py
@@ -5,18 +5,21 @@ register = template.Library()
 
 @register.filter
 def is_teacher(user):
+    """Returns True/False depending on if the user is a teacher"""
     if user.is_superuser:
         return True
     return user.groups.filter(name='Teacher').exists()
 
 @register.filter
 def is_superuser(user):
+    """Returns True/False depending on if the user is a superuser"""
     if user.is_superuser:
         return True
     return False
 
 @register.filter
 def get_rating(user):
+    """Returns the users rating"""
     u = User.objects.get(username=user.username)
     rating = u.extendeduser.rating
     return rating
