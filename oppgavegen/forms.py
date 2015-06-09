eaab16c9fa64b6b53e62a91ab5789f460061d5f8
Eirik Korsnes <korsneseirik@gmail.com>
2015-06-09 16:54:29 +0200

    wip ajax search, wip forms and wip template files for forms

--- oppgavegen/forms.py
+++ oppgavegen/forms.py
@@ -1,2 +1,14 @@
-## Define forms here
-## Forms etter models kommer her!
+from django.forms import ModelForm
+from .models import Set
+from .models import Level
+from .models import Template
+
+class SetForm(ModelForm):
+    class Meta:
+        model = Set
+        fields = ('name', 'chapter')
+
+class Level(ModelForm):
+    class Meta:
+        model = Level
+        fields = ('name', 'template')
