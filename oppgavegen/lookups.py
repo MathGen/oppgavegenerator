7752921d9f6286dce3aabb861a93ecca0b8fa83a
Eirik Korsnes <korsneseirik@gmail.com>
2015-06-16 19:19:01 +0200

    wip ajax tag search

---
+++ oppgavegen/lookups.py
@@ -0,0 +1,10 @@
+from .models import Template
+from selectable.base import ModelLookup
+from selectable.registry import registry
+
+
+class TemplateLookup(ModelLookup):
+    model = Template
+    search_fields = ('tags__name__icontains')
+
+registry.register(TemplateLookup)
