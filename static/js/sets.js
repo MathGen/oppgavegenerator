2c5aaaf1aac4856aa1973a13aa93778aac114349
w0pke <kenniii90@gmail.com>
2015-06-25 14:52:45 +0200

    Added jQuery-ui to the set-constructor.

--- static/js/sets.js
+++ static/js/sets.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     load_set_editor($('#set_id').text());
+    $('#chapter_container').sortable({placeholder:"list_chapter"}).disableSelection();
+    $('.list_chapter').draggable({containment:"#chapter_container", axis:"y"});
 });
 
 function load_set_editor(set_id){
