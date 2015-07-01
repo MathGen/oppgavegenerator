addf7d9d14aaabe29768d8957e26dba1a0fac791
einar <qeinar@gmail.com>
2015-07-01 15:24:33 +0200

    added simplify to answer checker.

--- oppgavegen/view_logic/answer_checker.py
+++ oppgavegen/view_logic/answer_checker.py
@@ -51,8 +51,10 @@ def parse_using_sympy(s):
 def parse_using_sympy_simplify(s):
     transformations = standard_transformations + (convert_xor, implicit_multiplication_application,)
     s = s.split('==')
+    print(s)
     new_s = []
     for x in s:
-        new_s.append(simplify(parse_expr(x, transformations=transformations, global_dict=None, evaluate=True)))
+        new_s.append(str(simplify(parse_expr(x, transformations=transformations, global_dict=None, evaluate=True))))
+    print(new_s)
     new_s = '=='.join(new_s)
     return parse_expr(new_s, transformations=transformations, global_dict=None, evaluate=True)
