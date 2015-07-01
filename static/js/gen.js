fd93fa7172508a4cecb2d987cd1d56687964547f
w0pke <kenniii90@gmail.com>
2015-07-01 14:43:06 +0200

    Small fix where an empty tag was inserted when a template had no tags under editing.

--- static/js/gen.js
+++ static/js/gen.js
@@ -1760,9 +1760,12 @@ function insert_editable_data(){
 
 	// Get and insert tags
 	var tags = JSON.parse($('#get_tags').text());
-	for(var t = 0; t < tags.length; t++){
-		$('#tags').prepend('<span class="tag">'+tags[t]+'<a class="btn btn_tag_del">x</a></span>');
+	if(tags[0] != ""){
+		for(var t = 0; t < tags.length; t++){
+			$('#tags').prepend('<span class="tag">'+tags[t]+'<a class="btn btn_tag_del">x</a></span>');
+		}
 	}
+
 	VAR_INIT = false;
 	redraw_mathquill_elements();
 }
