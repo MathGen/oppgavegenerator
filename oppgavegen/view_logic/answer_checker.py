e0d113323fb8184bbc84e26b6e7ac61f98e065e2
einar <qeinar@gmail.com>
2015-07-06 15:11:50 +0200

    Trying to fix margin of error.

--- oppgavegen/view_logic/answer_checker.py
+++ oppgavegen/view_logic/answer_checker.py
@@ -30,7 +30,7 @@ def check_answer(user_answer, answer, template_type, margin_for_error=0):
             for us in user_answer:
                 if margin_for_error != 0 and margin_for_error is not None and margin_for_error != '':
                     if parse_using_sympy_simplify(latex_to_sympy(us + ' + ' + margin_for_error) + '<=' + latex_to_sympy(s) +
-                                         '<=' + latex_to_sympy(us + ' + '+ margin_for_error)):
+                                         '<=' + latex_to_sympy(us + ' - '+ margin_for_error)):
                         user_answer.remove(us)
                         break
                 elif parse_using_sympy_simplify(latex_to_sympy(s) + '==' + latex_to_sympy(us)):
