696194f280156606423d9533b366093545d5abad
einar <qeinar@gmail.com>
2015-08-19 11:09:10 +0200

    small changes to the logic for when required and cheat check are run.

--- oppgavegen/views/views.py
+++ oppgavegen/views/views.py
@@ -154,7 +154,7 @@ def answers(request, level=1):
                     (form_values['template_type'] == 'normal') and (context_dict['user_won']):
                 context_dict['answer'] = cheat_message
                 return render_to_response(render_to, context_dict, context)
-            if (required_check(user_answer, required, form_values['variable_dictionary'].split('§'))) and \
+            elif (required_check(user_answer, required, form_values['variable_dictionary'].split('§'))) and \
                     (form_values['template_type'] == 'normal') and (context_dict['user_won']):
                 context_dict['answer'] = required_message
                 return render_to_response(render_to, context_dict, context)
